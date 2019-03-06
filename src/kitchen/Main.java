fdf8de6add2b2508497276133ca1f43f9b328a7a
ann <anngurjeva@gmail.com>
2019-03-06 13:08:17 +0200

    get Set <Cookers>, Set <MenuPoint>

--- src/kitchen/Main.java
+++ src/kitchen/Main.java
@@ -1,10 +1,7 @@
 package kitchen;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
 
@@ -27,59 +24,83 @@ public class Main {
     }
 
     private static void initKitchen(String cookersData, String menuData) {
-        String[] splitedCookerData = cookersData.split("\n");
-        String day = splitedCookerData[1];
+        Map<String, Set<String>> cookersDataMap = initDataByDay(cookersData);
+        Map<String, Set<String>> menuDataMap = initDataByDay(menuData);
         Map<String, Day> days = new HashMap<>();
-        Map<String, List<Cooker>> cookersBySpecialization = initCookers(splitedCookerData);
-
-/*        List<Cooker> cookers = new ArrayList<>();
-        for (int i = 2; i < splitedCookerData.length; i++) {
-            if (splitedCookerData[i].contains(":")) {
-                String[] cookerData = splitedCookerData[i].split(":");
-                cookers.add(new Cooker(
-                        cookerData[0].split(" ")[0],
-                        cookerData[0].split(" ")[1],
-                        cookerData[1]));
-            } else {
-                Map<String, List<Cooker>> cookersBySpecialization = new HashMap<>();
-                for (Cooker cooker : cookers) {
-                    if (cookersBySpecialization.containsKey(cooker.getSpecialization())) {
-                        cookersBySpecialization.get(cooker.getSpecialization()).add(cooker);
-                    } else {
-                        cookersBySpecialization.put(cooker.getSpecialization(), List.of(cooker));
-                    }
-                }
-                days.put(day, new Day(cookersBySpecialization));
-            }*/
-        }
+        // Map<String, Set<Cooker>> cookersBySpecialization = initCookers(splitedCookerData);
         new Kitchen(days);
     }
 
-    private static Map<String, List<Cooker>> initCookers(String[] splitedCookerData) {
-        Map<String, List<Cooker>> cookersBySpecialization = new HashMap<>();
-        List<Cooker> cookers = new ArrayList<>();
-        for (int i = 2; i < splitedCookerData.length; i++) {
+    private static Map<String, Set<String>> initDataByDay(String data) {
+        String[] splitedCookerData = data.split("\n");
+        String day = splitedCookerData[0];
+        Map<String, Set<String>> result = new HashMap<>();
+        Set<String> dataSet = new HashSet<>();
+        for (int i = 1; i < splitedCookerData.length; i++) {
             if (splitedCookerData[i].contains(":")) {
-                String[] cookerData = splitedCookerData[i].split(":");
-                cookers.add(new Cooker(
-                        cookerData[0].split(" ")[0],
-                        cookerData[0].split(" ")[1],
-                        cookerData[1]));
+                dataSet.add(splitedCookerData[i]);
             } else {
-                for (Cooker cooker : cookers) {
-                    if (cookersBySpecialization.containsKey(cooker.getSpecialization())) {
-                        cookersBySpecialization.get(cooker.getSpecialization()).add(cooker);
-                    } else {
-                        cookersBySpecialization.put(cooker.getSpecialization(), List.of(cooker));
-                    }
-                }
-//                days.put(day, new Day(cookersBySpecialization));
+                result.put(day, dataSet);
+                dataSet = new HashSet<>();
             }
-            return cookersBySpecialization;
         }
-
-        private static void initMenuPoints(String[] splitedCookerData) {
-
-        }
-
+        return result;
     }
+
+    public static Map<String, Set<Cooker>> initCookers(Set<String> cookerData) {
+        List<Cooker> cookers = new ArrayList<>();
+        for (String cooker : cookerData) {
+            String[] splitedCookerData = cooker.split(":");
+            cookers.add(new Cooker(
+                    splitedCookerData[0].split(" ")[0],
+                    splitedCookerData[0].split(" ")[1],
+                    splitedCookerData[1]));
+        }
+        return getCookerPointBySpecialization(cookers);
+    }
+
+    public static Map<String, Set<MenuPoint>> initMenuPoints(Set<String> menuData) {
+        List<MenuPoint> menuPoints = new ArrayList<>();
+        for (String menuPoint : menuData) {
+            String[] splitedMenuData = menuPoint.split(":");
+            menuPoints.add(new MenuPoint(
+                    splitedMenuData[0],
+                    getPriceFromString(splitedMenuData[1]),
+                    splitedMenuData[2]));
+        }
+        return getMenuPointBySpecialization(menuPoints);
+    }
+
+    private static Map<String, Set<MenuPoint>> getMenuPointBySpecialization(List<MenuPoint> menuPoints) {
+        Map<String, Set<MenuPoint>> menuBySpecialization = new HashMap<>();
+        for (MenuPoint menuPoint : menuPoints) {
+            if (menuBySpecialization.containsKey(menuPoint.getSpecialization())) {
+                menuBySpecialization.get(menuPoint.getSpecialization()).add(menuPoint);
+            } else {
+                menuBySpecialization.put(menuPoint.getSpecialization(), Set.of(menuPoint));
+            }
+        }
+        return menuBySpecialization;
+    }
+
+    private static Map<String, Set<Cooker>> getCookerPointBySpecialization(List<Cooker> cookers) {
+        Map<String, Set<Cooker>> cookersBySpecialization = new HashMap<>();
+        for (Cooker cooker : cookers) {
+            if (cookersBySpecialization.containsKey(cooker.getSpecialization())) {
+                cookersBySpecialization.get(cooker.getSpecialization()).add(cooker);
+            } else {
+                cookersBySpecialization.put(cooker.getSpecialization(), Set.of(cooker));
+            }
+        }
+        return cookersBySpecialization;
+    }
+
+    private static double getPriceFromString(String stringPrice) {
+        String[] prices = stringPrice.split(" ");
+        return Double.parseDouble(prices[0].replace("грн", ""))
+                + Double.parseDouble(prices[1].replace("коп", "")) * 0.01;
+    }
+
+
+}
+
